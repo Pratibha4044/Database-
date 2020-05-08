90d8484dd6624f007b05a4dc0dc93728369f6ca1
Pratibha Patil <pratibha.rampatil@gmail.com>
2020-05-08 22:30:49 +0530

    Added exception handling

--- src/database/Main.java
+++ src/database/Main.java
@@ -8,16 +8,22 @@ public static void main(String[] args) {
         password = "";
         Dbconnect connect = new Dbconnect(dbname, username, password);
         ResultSet res = connect.getData("select  * from doctors");
-        System.out.println("Records from database");
-        while (res.next()){ 
-        // checks all the entries from the DB (will not enter while loop if no entries)
-			int Doctor_id= res.getInt("Doctor_id");
-			String Name = res.getString("Name");
-			String Assistant_name = res.getString("Assistant_name");
-			String Specialisation= res.getString("Specialisation");
-			int Contact_no= res.getInt("Contact_no");
-			int Visiting_hours= res.getInt("Visiting_hours");
-			System.out.printf("Doctor_id : %s\nName: %s\nAssistant_name : %s\nSpecialisation : %s\nContact_no : %s\nVisiting_hours : %s\n",Doctor_id,Name,Assistant_name,Specialisation,Contact_no,Visiting_hours );
+        try {
+            System.out.println("Records from database: ");
+            while (res.next()){ 
+            // checks all the entries from the DB (will not enter while loop if no entries)
+                int Doctor_id= res.getInt("Doctor_id");
+                String Name = res.getString("Name");
+                String Assistant_name = res.getString("Assistant_name");
+                String Specialisation= res.getString("Specialisation");
+                int Contact_no= res.getInt("Contact_no");
+                int Visiting_hours= res.getInt("Visiting_hours");
+                System.out.printf("Doctor_id : %s\nName: %s\nAssistant_name : %s\nSpecialisation : %s\nContact_no : %s\nVisiting_hours : %s\n",Doctor_id,Name,Assistant_name,Specialisation,Contact_no,Visiting_hours );
+            }
+        } catch (SQLException ex) {
+            System.out.println("Fetching error: ");
+            ex.printStackTrace();
+        }
         }
     }
 }
