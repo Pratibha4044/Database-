b9ca8deadbac65a8950993d8dcf7df29442c55f6
Pratibha Patil <pratibha.rampatil@gmail.com>
2020-05-08 20:40:10 +0530

    heii

---
+++ src/Dbconnect.java
@@ -0,0 +1,46 @@
+import java.sql.*;
+
+public class Dbconnect {
+    
+private String dbname, username, password;
+    private Connection con;
+    private Statement stmt;
+    private ResultSet res;
+    
+    public  Dbconnect(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            dbname = "jdbc:mysql://localhost:3306/hospital_managment_system";
+            username = "root";
+            password = "";
+            con = DriverManager.getConnection(dbname, username, password);
+        }catch(Exception ex){
+            System.out.println("Connection Error:");
+            ex.printStackTrace();
+        }
+    }
+    public void getData(){
+        try{
+            stmt = con.createStatement();
+            String query = "select  * from doctors";
+            res = stmt.executeQuery(query);
+            System.out.println("Records from database");
+            while (res.next()){
+                int Doctor_id= res.getInt("Doctor_id");
+                String Name = res.getString("Name");
+                 String Assistant_name = res.getString("Assistant name");
+                  String Specialisation= res.getString("Specialisation");
+                int Contact_no= res.getInt("Contact_no");
+                int Visiting_hours= res.getInt("Visiting_hours");
+              System.out.printf("Doctor_id : %s\nName: %s\nAssistant_name : %s\nSpecialisation : %s\nContact_no : %s\nVisiting_hours : %s\n",Doctor_id,Name,Assistant_name,Specialisation,Contact_no,Visiting_hours );
+            }
+        }catch (Exception ex){
+            System.out.println("Fetching Error:");
+            ex.printStackTrace();
+        }
+    }
+}
+
+
+
+
